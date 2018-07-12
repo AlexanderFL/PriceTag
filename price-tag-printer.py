92f30b161e04884b80322d59216964bcc24350ba
Alexander Freyr Lúðvíksson <alex98freyr@gmail.com>
2018-07-12 14:30:49 +0000

    Update price-tag-printer.py

--- price-tag-printer.py
+++ price-tag-printer.py
@@ -1,60 +1,63 @@
-# Python 3.6.4
+# Works with python version 3.6.4
 
-from openpyxl import load_workbook
-debug = True  # Setting debug to true will disable user input
+import win32com.client as win32
+from time import sleep
 
+main_file = "C:\\Users\\ht-afgr7\\Desktop\\PriceTag-master\\PriceTag-master\\test1.xlsm"
+label_file = "vorunr.txt"
+label_type = "A6L"
 
-def is_debug():
-    return debug
+def reset_label_column(sheet, stop=14):
+    for x in range(2, stop):
+        sheet.Cells(x, 1).Value = None
 
+label_number = 0
+def get_label_number():
+    if label_type is "VerdmidarLitlir":
+        label_number = 12
+    else if label_type is "Verdmidar":
+        label_number = 12
+    else if label_type is "A7":
+        label_number = 8
+    else if label_type is "A6":
+        label_number = 4
+    else if label_type is "A6L":
+        label_number = 2
+    else if label_type is "A6B":
+        label_number = 4
+    else if label_type is "A5":
+        label_number = 2
 
-class Labels:
-    hillumidi = 16
-    stormidi = 2
-    a6b = 4
+# Opening excel and setting the active worksheet
+excel = win32.gencache.EnsureDispatch('Excel.Application')
+excel.Interactive = True
+excel.Visible = True
+excel.DisplayAlerts = True
 
+xlBook = excel.Workbooks.Open(main_file)
+xlSheet = xlBook.Worksheets('Front')
 
-nr_of_labels = 16
-if not is_debug():
-    nr_of_labels = int(input(": "))
-
-label_type = Labels.stormidi
-
-code_filename = 'vorunr.txt'
-excel_filename = 'test1.xlsm'
-
-wb = None
-
-try:
-    wb = load_workbook(excel_filename, keep_vba=True)
-except PermissionError:
-    print("Access was denied, is the file open? If so, close the file and try again.")
-    exit()
-except FileNotFoundError:
-    print("File '" + excel_filename + "' was not found.")
-    exit()
-
-ws = wb.active
-
-file = open(code_filename, "r")
+# Open label file
 codes = []
-for line in file:
-    codes.append(line)
-print(codes)
+with open(label_file) as f:
+    codes = f.read().splitlines() 
 
+# Placing all of the labels and printing
+label_type = 12
 label_count = 0
 label_sub_count = 0
 while label_count != len(codes):
-    if label_sub_count % label_type == 0 and label_count is not 0:
+    if label_sub_count % 2 == 0 and label_count is not 0:
         label_sub_count = 0
-        print("printing")
-    col_string = 'A' + str(2+label_sub_count)
-    print(col_string)
-    ws[col_string] = codes[label_count]
+        xlSheet = xlBook.Worksheets(label_type)
+        # xlSheet.PrintOut(Copies=1, Collate=True)
+        sleep(1)
+        xlSheet = xlBook.Worksheets('Front')
+        reset_label_column(xlSheet, 2+2)
+    xlSheet.Cells(label_sub_count+2,1).Value = codes[label_count]
 
     label_sub_count += 1
     label_count += 1
-print("printing")
-
-wb.template = False
-wb.save('test1.xlsm')
+xlSheet = xlBook.Worksheets(label_type)
+# xlSheet.PrintOut(Copies=1, Collate=True)
+xlSheet = xlBook.Worksheets('Front')
