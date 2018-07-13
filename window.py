d72132cfa0be8679760d153c2cbbc3be8998ff08
Alexander Freyr Lúðvíksson <alex98freyr@gmail.com>
2018-07-13 00:28:18 +0000

    Window

--- window.py
+++ window.py
@@ -2,37 +2,35 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.create_widgets()
+
         self.label_file_path = None
 
     def create_widgets(self):
-        verdmidalisti = ["Verdmidar", "VerdmidarLitlir", "A7", "A6", "A6L",
-                        "A6B", "A5"]
-        
-        self.label = ttk.Label(text="Verðmerkja", style="BW.TLabel")
-        self.label.grid(row=0, column=0)
+        verdmidautlit = ["Verdmidar", "VerdmidarLitlir", "A7", "A6", "A6L",
+                         "A6B", "A5"]
+        # Row 0
+        self.title = ttk.Label(text="Verðmiðar", font=("Roboto", 20))
+        self.title.place(x=720/2, y=25, anchor="center")
 
-        self.drop = ttk.Combobox(state="readonly", values=verdmidalisti)
-        self.drop.set("Veldu verðmiðaútlit")
-        self.drop.grid(row=1, column=0)
-        
-        self.hi_there = ttk.Button(text="Test", width=10, command=self.get_label_values)
-        self.hi_there.grid(row=2, column=0)
+        # Row 1
+        self.label = ttk.Label(text="Veldu verðmiðaútlit: ", style="BW.TLabel")
+        self.label.place(x=25, y=50)
 
-        self.quit = ttk.Button(text="QUIT", width=10, command=root.destroy)
-        self.quit.grid(row=3, column=0)
-
-        self.input = ttk.Entry(width=50, state="normal")
-        self.input.grid(row=3, column=3)
+        self.drop = ttk.Combobox(state="readonly", values=verdmidautlit)
+        self.drop.set("Verdmidar")
+        self.drop.place(x=150, y=50)
 
     def say_hi(self, box):
         print(box.get())
 
     def get_label_values(self):
         self.label_file_path = askopenfilename()
+
 
 root = tk.Tk()
 app = Application(master=root)
